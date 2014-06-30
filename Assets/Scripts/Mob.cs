246b92e189efd0f9d85aecb734e5f88c264c29cc
ChristianT <ChristianT@onlyentertainment.de>
2014-06-30 13:40:37 +0200

    FIrst Mob Classes

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour
+{
+
+    public int moneyDrop;
+    public int experienceDrop;
+    public int killsPlayerLive = 1;
+
+    public float maxHealth;
+    public float curHealth;
+
+    public float maxShield;
+    public float curShield;
+
+    public float maxSpeed;
+    public float curSpeed;
+
+    public float maxArmor;
+    public float curArmor;
+
+    public bool isFlying = false;
+
+    public float regenerationHealth;
+    public float regenerationShield;
+    public float regenerationArmor;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        curHealth = Mathf.Clamp(curHealth + regenerationHealth, 0, maxHealth);
+        curShield = Mathf.Clamp(curShield + regenerationShield, 0, maxShield);
+        curArmor = Mathf.Clamp(curArmor + regenerationArmor, 0, maxArmor);
+
+    }
+
+
+    void TakeDamage(float damageAmount)
+    {
+        float damageToShield = Mathf.Clamp(damageAmount, 0, curShield);
+        curShield = Mathf.Clamp(curShield - damageToShield, 0, curShield);
+        damageAmount = Mathf.Clamp(damageAmount - damageToShield, 0, damageAmount);
+
+        float damageToArmor = Mathf.Clamp(damageAmount, 0, curArmor);
+        curArmor = Mathf.Clamp(curArmor - damageToArmor, 0, curArmor);
+        damageAmount = Mathf.Clamp(damageAmount - damageToArmor, 0, damageAmount);
+
+        curHealth = Mathf.Clamp(curHealth - damageAmount, 0, curHealth);
+
+        if (curHealth <= 0) Destroy(this);
+
+    }
+
+}
---
+++ Assets/Scripts/Mob_Movement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob_Movement : MonoBehaviour
+{
+
+    public enum ACTIONS { Idle, Move };
+
+    public ACTIONS action = ACTIONS.Idle;
+
+    private Mob mobSettings;
+
+    public Vector2 moveTo;
+    public ArrayList path;
+    public Waypoint startWaypoint;
+    public Waypoint endWaypoint;
+    public int curWaypoint;
+
+    void Awake()
+    {
+        mobSettings = gameObject.GetComponent<Mob>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 curPosition = new Vector2(transform.position.y, transform.position.z);
+
+        if (path != null)
+        {
+            if (curWaypoint + 1 == path.Count && action == ACTIONS.Idle)
+            {
+                curWaypoint = 0;
+                path = null;
+            }
+            else
+            {
+                if (action == ACTIONS.Idle)
+                {
+                    Waypoint nextWaypoint = (Waypoint)path[curWaypoint];
+                    moveTo = nextWaypoint.transform.position;
+                    action = ACTIONS.Move;
+                    curWaypoint++;
+                }
+                else if (action == ACTIONS.Move)
+                {
+                    if ((moveTo - curPosition).magnitude >= mobSettings.curSpeed * Time.deltaTime)
+                    {
+                        transform.LookAt(new Vector3(moveTo.x, transform.position.y, moveTo.y));
+                        transform.Translate(Vector3.forward * mobSettings.curSpeed * Time.deltaTime);
+                        //transform.position=(new Vector3(tran
+                    }
+                    else { action = ACTIONS.Idle; }
+                }
+            }
+
+        }
+    }
+
+    private void MoveToPoint(Vector3 moveTo)
+    {
+        //action = ACTIONS.Move;
+        //this.moveTo = new Vector2(moveTo.x, moveTo.z);
+    }
+
+    public void StartMoving(Waypoint startWaypoint, Waypoint endWaypoint)
+    {
+        this.startWaypoint = startWaypoint;
+        this.endWaypoint = endWaypoint;
+
+        path = PathFinder.GetPath(startWaypoint, endWaypoint);
+        curWaypoint = 0;
+        action = ACTIONS.Idle;
+    }
+
+
+}
