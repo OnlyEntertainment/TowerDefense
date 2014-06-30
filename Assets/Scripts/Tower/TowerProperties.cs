3acf5647a7cea997997ceb365cc6b41cc963382b
khuser <koemeterion@googlemail.com>
2014-06-30 14:48:02 +0200

    gnarf

--- Assets/Scripts/Tower/TowerProperties.cs
+++ Assets/Scripts/Tower/TowerProperties.cs
@@ -9,7 +9,7 @@ public class TowerProperties : MonoBehaviour {
     // ------------
 
     public string towerName = "normal";         // Tower Name
-    public float towerDamage = 5f;              // ausgeteilter Schaden
+    public float towerDamage = 15f;              // ausgeteilter Schaden
     public int towerCosts = 100;                // Tower Kauf kosten
     public float towerMoveSpeed = 3f;           // Drehgeschwindigkeit des Towers
     public float towerShotFrequence = 2f;       // Schussfrequenz
