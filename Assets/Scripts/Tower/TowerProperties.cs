690a2f62fa0ef8bedc3aa84707a6154628a1727d
khuser <koemeterion@googlemail.com>
2014-06-30 11:20:58 +0200

    Upload zeroPointOne

---
+++ Assets/Scripts/Tower/TowerProperties.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerProperties : MonoBehaviour {
+
+    // ############
+    // Variablen
+    // ------------
+
+    public string towerName = "normal";         // Tower Name
+    public float towerDamage = 5f;              // ausgeteilter Schaden
+    public int towerCosts = 100;                // Tower Kauf kosten
+    public float towerMoveSpeed = 3f;           // Drehgeschwindigkeit des Towers
+    public float towerShotFrequence = 2f;       // Schussfrequenz
+
+
+
+    // ############
+    // Methoden
+    // ------------
+
+	void Start () 
+    {
+	
+	} // END Start
+	
+	void Update () 
+    {
+	
+	} // END Update
+} // END Class
