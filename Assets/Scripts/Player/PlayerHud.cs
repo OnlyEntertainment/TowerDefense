8a61e18d4257d9597a99f6cc8cf4a7bc079aeba3
khuser <koemeterion@googlemail.com>
2014-07-03 19:26:24 +0200

    hu

---
+++ Assets/Scripts/Player/PlayerHud.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHud : MonoBehaviour {
+
+    public PlayerKeyBinding playerKeyBinding;
+    
+    
+    
+    public int playerMoney;
+    public float playerHealth;
+
+
+    public bool menuChoice = false;             // true - Baumodus | false - Kampfmodus
+
+    public Texture2D iconSword;
+    public Texture2D iconGun;
+    public Texture2D iconTower;
+    public Texture2D iconWall;
+
+	void Start () 
+    {
+	
+	} // END Start
+	
+	void Update () 
+    {
+
+        if (Input.GetKeyDown(playerKeyBinding.menuChangeChoice))
+        {
+            if (menuChoice == false)
+            {
+                menuChoice = true;
+            }
+            else
+            {
+                menuChoice = false;
+            }
+        }
+
+
+	} // END Update
+
+
+    void OnGUI()
+    {
+
+        if (menuChoice == false)
+        {
+            GUI.Box(new Rect(5, 5, 100, 100), iconSword);
+            GUI.Box(new Rect(115, 5, 100, 100), iconGun);
+
+            GUI.Box(new Rect(5, 115, 100, 100), "Fight");
+
+        }
+        else
+        {
+            GUI.Box(new Rect(5, 5, 100, 100), iconTower);
+            GUI.Box(new Rect(115, 5, 100, 100), iconWall);
+
+            GUI.Box(new Rect(5, 115, 100, 100), "Build");
+        }
+
+
+        GUI.Label(new Rect(5, Screen.height - 50, 200, 20), "Money: " + playerMoney);
+        GUI.Label(new Rect(5, Screen.height - 20, 200, 20), "Health: " + playerHealth);
+    } // END OnGUI
+
+
+
+
+} // END Class
---
+++ Assets/Scripts/Player/PlayerKeyBinding.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKeyBinding : MonoBehaviour 
+{
+
+    public KeyCode moveForward = KeyCode.W;
+    public KeyCode moveBackward = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode moveRun = KeyCode.LeftShift;
+    public KeyCode moevJump = KeyCode.Space;
+
+    public KeyCode menuChangeChoice = KeyCode.Q;
+    public KeyCode menuFightChoice1 = KeyCode.Alpha1;
+    public KeyCode menuFightChoice2 = KeyCode.Alpha2;
+    public KeyCode menuBuildChoice1 = KeyCode.Alpha1;
+    public KeyCode menuBuildChoice2 = KeyCode.Alpha2;
+    
+    // TODO: Linke Maustaste
+    // TODO: Rechte Maustaste
+} // END Class
